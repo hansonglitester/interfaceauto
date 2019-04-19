29d522d4481e1d83bfef5c74907e4c4c1dd47895
tester <2275057986@qq.com>
2019-04-19 18:01:44 +0800

    2019-tijiao

--- interfaceauto/src/main/java/com/hsl/cn/Application.java
+++ interfaceauto/src/main/java/com/hsl/cn/Application.java
@@ -2,10 +2,11 @@ package com.hsl.cn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = MongoAutoConfiguration.class)
 public class Application {
-    public static void main(String[] args) {
+   public static void main(String[] args) {
         SpringApplication.run(Application.class,args);
     }
 }
--- interfaceauto/src/main/java/com/hsl/cn/testcase/TestUserDetailCase2.java
+++ interfaceauto/src/main/java/com/hsl/cn/testcase/TestUserDetailCase2.java
@@ -1,83 +1,66 @@
 package com.hsl.cn.testcase;
 
 
-import com.hsl.cn.config.BaseTest;
 import com.hsl.cn.model.UserDetailCase;
 import com.hsl.cn.repository.UserDetailCaseDao;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
 import java.util.*;
 
-@SpringBootTest
+
 public class TestUserDetailCase2  extends BaseTest {
+
+    Logger logger = LoggerFactory.getLogger(TestUserDetailCase2.class);
+    Logger logger1=LoggerFactory.getLogger(TestUserDetailCase2.class);
+
     @Autowired
     UserDetailCaseDao userDetailCaseDao;
 
-    @DataProvider(name="userdetaildata")
+    @DataProvider(name="data")
     public Iterator<Object[]> getdata(){
 
-        List<UserDetailCase> list= userDetailCaseDao.findAll();
-        System.out.println(list);
-
+        List<UserDetailCase> userlist= userDetailCaseDao.findAll();
+        System.out.println(userlist);
+        Iterator it=userlist.iterator();
         List<Object[]> result=new ArrayList<>();
-        Iterator it=list.iterator();
         while(it.hasNext()){
             result.add(new Object[] { it.next() });
         }
-        return  result.iterator();
-    }
 
-
-    @DataProvider(name="userdetaildata2")
-    public Object [][] getdata2() {
-        UserDetailCase u1=userDetailCaseDao.findByUserId(1);
-        System.out.println(u1.toString()+"111111111111111111");
-        Object[][] data =new Object[][] {
-                {u1}
-        };
-        return data;
+        return   result.iterator();
     }
 
 
 
-    @DataProvider(name="userdetaildata3")
-    public Object [][] getdata3() {
-        Map <String ,String> map =new HashMap <>();
-        map.put("id","1");
-        map.put("userid","1");
-        Object[][] data=new Object[][]{
-                {map}
-        };
-        return data;
+    @DataProvider(name="data2")
+    public Object[][] getdata2(){
+
+        List<UserDetailCase> userlist= userDetailCaseDao.findAll();
+        logger.info(userlist.toString());
+
+        //定义二位数组，用于返回测试数据
+        Object[][] result =new Object[userlist.size()][];
+
+        for (int i=0;i<userlist.size();i++){
+
+            result[i]=new Object[]{userlist.get(i)};
+        }
+        return  result;
     }
 
-    @Test(dataProvider = "userdetaildata2")
+
+    @Test(dataProvider = "data2")
     public void TestUserList2(UserDetailCase userDetailCase) throws IOException {
+        logger.info(userDetailCase.toString());
         System.out.println(userDetailCase.toString());
         //组参数
         Map <String ,Object> map =new HashMap<String, Object>();
-//
-//        //发请求,返回响应的内容
-//        String  result= HttpClientUtils.sendGet(Config.getUserDetailUrl,map);
-//
-//        //解析断言
-//        Gson gson=new Gson();
-//        //对象转成json串 gson.toJson(result);
-//       Map<String,Object > resultjson =gson.fromJson(result,Map.class);
-//       for (Map.Entry<String ,Object> entry:resultjson.entrySet() ) {
-//           System.out.println(entry.getKey()+":"+entry.getValue());
-//       }
 
-
-
-    }
-    @Test(dataProvider = "userdetaildata3")
-    public void TestUserList3(Map<String,String> map) throws IOException {
-        System.out.println(map.toString());;
 
     }
 
